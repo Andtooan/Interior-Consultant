d7106070a0f7753ccd6e86293daa162937510d73
Antonio DroCode <72485462+Andtooan@users.noreply.github.com>
2021-07-03 13:33:06 -0500

    Add files via upload

---
+++ dummy.js
@@ -0,0 +1,38 @@
+const data = [
+    { 
+        name: 'Proyecto 1',
+        horas: 40
+    }, 
+    {
+        name: 'Proyecto 2',
+        horas: 30,
+    },
+    {
+        name: 'Proyecto 3',
+        horas: 20,
+    },
+    ]; 
+
+    const grap = [
+        {
+            name: 'Proyecto 1',
+            uv: 12,
+            fill: '#8884d8',
+        },
+        
+        {
+            name: 'Proyecto 2',
+            uv: 10.22,
+            fill: '#82ca9d',
+        },
+        {
+            name: 'Proyecto 3',
+            uv: 9,
+            fill: '#a4de6c',
+        },
+        {
+            name: 'Proyecto 4',
+            uv: 11,
+            fill: '#d0ed57',
+        },
+        ];
